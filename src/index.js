2b333506f4f13428da62bac3fe79f92028e94f69
NataSemenuck-BELITSOFT <nata.semenuck@gmail.com>
2019-09-18 14:56:47 +0300

    Added links

--- src/index.js
+++ src/index.js
@@ -1,46 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import App from './components/App/App'
+ReactDOM.render(<App />, document.getElementById('root'));
 
-class SwapiService {
-    _apiBase = 'https://swapi.co/api';
-    async getResourse(url) {
-        const res = await fetch(`${this._apiBase}${url}`);
-        if(!res.ok) {
-            throw new Error(`Couldn\'t fetch ${this._apiBase}${url}` + `, received ${res.status}`);
-        }  
-        return await res.json();
-    }
 
-    async getAllPeople() {
-        const result = await this.getResourse('/people/');
-        return result.results;
-    };
-    getPersone(id) {
-        return this.getResourse(`/people/${id}`);
-    };
-
-    async getAllPlanets(){
-        const result = await this.getResourse('/planets/');
-        return result.results;
-    };
-    getPlanet(id) {
-        return this.getResourse(`/planets/${id}`);
-    };
-
-    async getAllStarships() {
-        const result = await this.getResourse('/starships/');
-        return result.results;
-    };
-    getStarship(id) {
-        return this.getResourse(`/starships/${id}`);
-    };
-}
-
-const swapi = new SwapiService();
-swapi.getAllStarships().then((body) => {
-    body.forEach(element => {
-        console.log(element.name);
-    });
+// const swapi = new SwapiService();
+// swapi.getAllStarships().then((body) => {
+//     body.forEach(element => {
+//         console.log(element.name);
+//     });
     
-});
-swapi.getStarship(5).then((body) => {
-    console.log(body.thenname);
-})
+// });
+// swapi.getStarship(5).then((body) => {
+//     console.log(body.thenname);
+// })
