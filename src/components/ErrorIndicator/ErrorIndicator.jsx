d9ca0f765feeb5ba838ebcb3dba18eb3491c0100
NataSemenuck-BELITSOFT <nata.semenuck@gmail.com>
2019-10-01 19:33:13 +0300

    Changed styles for ErrorIndicator

--- src/components/ErrorIndicator/ErrorIndicator.jsx
+++ src/components/ErrorIndicator/ErrorIndicator.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './ErrorIndicator.css';
-import image from './imageRocket.png';
+import image from './imageError.svg';
 const ErrorIndicator = () => {
     return (
         <div className = 'box'>
             <img className = 'imageForError' src = { image }/>
             <div>
-                <span className = 'glitch'>Boom!</span>
-                <span className = ''>Something is wrong</span>
-                <span className = ''>Don't worry!</span>
-                <span className = ''>Druids are on the way:)</span>
+                <span>Ooops...</span>
+                <span>Something is wrong.</span>
+                <span>Don't worry!</span>
+                <span>Druids are on the way:)</span>
             </div>
         </div>
     );
